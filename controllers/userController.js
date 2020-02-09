58e230ca228adf12a7ebf9f2bd860090326dccbc
phenix1229 <paul.garay@codeimmersives.com>
2020-02-09 16:17:38 -0500

    done

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ module.exports = {
     getUserById:(req, res) => {
         const userExists = users.filter(user => user.id === req.params.id);
         if(userExists.length > 0){
-            return res.status(200).json(userExists[0]);
+            return res.status(200).json({message:'getUserById'});
         } else {
             return res.status(400).json({message: `User with id:${req.params.id} does not exist`});
         }
@@ -22,12 +22,31 @@ module.exports = {
         newUser.name = req.body.name;
         newUser.email = req.body.email;
         users.push(newUser);
-        return res.json(req.body);
+        return res.json({message:'createUser'});
     }, 
-    // updateUser:(req, res) => {
-        
-    // },
-    // deleteUser:(req, res) => {
-        
-    // }
+    updateUser:(req, res) => {
+        const userExists = users.filter(user => user.id === req.params.id);
+        if(userExists.length > 0){
+            const updatedUser = req.body;
+            const user = userExists[0];
+            if(user.id === req.params.id){
+                user.name = updatedUser.name ? updatedUser.name : user.name;
+                user.email = updatedUser.email ? updatedUser.email : user.email;
+                return res.status(200).json({message: 'updateUser'});
+            }
+        } else {
+            return res.status(400).json({message: `User with id: ${req.params.id} does not exist`})
+        }
+    },
+    deleteUser:(req, res) => {
+        const userExists = users.filter(user => user.id === req.params.id);
+        const user = userExists[0];
+        if(userExists.length > 0){
+            const deletedUser = users.indexOf(user);
+            users.splice(deletedUser, 1);
+            return res.status(200).json({message: 'deleteUser'})
+        } else {
+            return res.status(400).json({message: `User with id: ${req.params.id} does not exist`})
+        }
+    }
 }
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-// const path = require('path');
-// const users = require('./models/users')
 const port = process.env.PORT || '3000';
 const logger = require('morgan');
 const userRoutes = require('./routes/router');
@@ -9,7 +7,6 @@ const userRoutes = require('./routes/router');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-// app.use(express.static('/api/users'));
 app.use('/api/users', userRoutes);
 
 
